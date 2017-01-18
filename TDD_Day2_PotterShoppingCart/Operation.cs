a88c0bb75844e60c35e46b6d9e19b81b2624f13f
GabyJ <asdf1@gmail.com>
2017-01-18 23:22:42 +0800

    重構_  一二集各買了一本，第三集買了兩本，價格應為100*3*0.9 + 100 = 370

--- TDD_Day2_PotterShoppingCart/Operation.cs
+++ TDD_Day2_PotterShoppingCart/Operation.cs
@@ -9,8 +9,27 @@ namespace TDD_Day2_PotterShoppingCart
     {
         public int CalcBook(IEnumerable<Book> books)
         {
-               
-            return Convert.ToInt32(books.Sum(o=>o.Price)*CalcPrice(books.Count()));
+            int Price = 0;
+            var _books = new List<Book>();
+
+            //Quantity本數至少大於一本
+            while (books.Any(x => x.Quantity > 0))
+            {
+                foreach (var book in books)
+                {
+                    //算出折扣金額
+                    Price += Convert.ToInt32(book.Price * CalcPrice(books.Count()));
+
+                    book.Quantity = book.Quantity - 1;
+                    //如果Quantity不只一本，將加入book原價
+                    if (book.Quantity > 0)
+                    {
+                        _books.Add(book);
+                    }
+                    Price += CalcBook(_books);
+                }
+            }
+            return Price;
         }
         /// <summary>
         /// 依不同級別折扣
--- TDD_Day2_PotterShoppingCartTests/OperationTests.cs
+++ TDD_Day2_PotterShoppingCartTests/OperationTests.cs
@@ -105,5 +105,24 @@ namespace TDD_Day2_PotterShoppingCart.Tests
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod]
+        public void Test_一二集各買一本_第三集買兩本()
+        {
+            //Arrange
+            Operation target = new Operation();
+            int expected = 370;
+
+            //Act
+            var Cart = new List<Book>();
+            Cart.Add(new Book { StortName = "Harry Potter_Episode1", Quantity = 1, Price = 100 });
+            Cart.Add(new Book { StortName = "Harry Potter_Episode2", Quantity = 1, Price = 100 });
+            Cart.Add(new Book { StortName = "Harry Potter_Episode3", Quantity = 2, Price = 100 });
+
+            var actual = target.CalcBook(Cart);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
